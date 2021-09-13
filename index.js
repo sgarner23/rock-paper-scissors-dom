31e564bf4f659c74cabfe07934e305e74f4318f8
Stephen Garner <stephenallen.garner@gmail.com>
2021-09-13 16:34:45 -0600

    completed rock, paper, and scissor buttons

---
+++ index.js
@@ -0,0 +1,110 @@
+
+//checking to make sure file is connected 
+console.log("Hello World");
+
+//defining global variables 
+let rockBtn = document.querySelector(".rock-button");
+let paperBtn = document.querySelector(".paper-button");
+let scissorsBtn = document.querySelector(".scissors-button");
+let playAgainBtn = document.querySelector(".play-button");
+
+let opponentText = document.getElementById("computer-choice-text");
+let winnerText = document.getElementById('winner-text');
+let playerText = document.getElementById('player-text');
+
+let computerChoice = "";
+
+
+//function to calculate computer choice 
+function getComputerChoice() {
+  let computerChoiceInt = Math.floor(Math.random() * 3);
+  let opponentChoice = "";
+
+  switch (computerChoiceInt) {
+    case 0:
+      opponentChoice = "Rock";
+      break;
+
+    case 1:
+      opponentChoice = "Paper";
+      break;
+
+    case 2:
+      opponentChoice = "Scissors";
+      break;
+    default:
+      console.log("Random number isn't working");
+  }
+  return opponentChoice;
+}
+
+//calculate who wins if user clicks rock 
+function rockChosen(e) {
+  computerChoice = getComputerChoice();
+  playerText.textContent = "Player chose Rock"
+  if (computerChoice === "Rock") {
+    opponentText.textContent = "Opponent chose Rock";
+    winnerText.textContent = "It's a tie!"
+
+  }
+
+  if (computerChoice === "Paper"){
+    opponentText.textContent = "Opponent chose Paper";
+    winnerText.textContent = "Opponent wins!"
+  }
+
+  if (computerChoice === 'Scissors'){
+    opponentText.textContent = "Opponent chose Scissors"
+    winnerText.innerHTML = "You win!!! &#128513"
+  }
+}
+
+//calculate who wins if user clicks paper 
+function paperChosen(e){
+  playerText.textContent = "Player chose Paper"
+  computerChoice = getComputerChoice();
+  if (computerChoice === "Rock") {
+    opponentText.textContent = "Opponent chose Rock";
+    winnerText.innerHTML = "You win!!! &#128513"
+
+  }
+
+  if (computerChoice === "Paper"){
+    opponentText.textContent = "Opponent chose Paper";
+    winnerText.textContent = "It's a tie!"
+  }
+
+  if (computerChoice === 'Scissors'){
+    opponentText.textContent = "Opponent chose Scissors"
+    winnerText.textContent = "Opponent wins!"
+  }
+}
+
+//calculate who wins if user clicks scissors 
+function scissorsChosen(e){
+  playerText.textContent = "Player chose Scissors"
+  computerChoice = getComputerChoice();
+  if (computerChoice === "Rock") {
+    opponentText.textContent = "Opponent chose Rock";
+    winnerText.textContent = "Opponent wins!"
+
+  }
+
+  if (computerChoice === "Paper"){
+    opponentText.textContent = "Opponent chose Paper";
+    winnerText.textContent = "You win!!!" 
+    winnerText.innerHTML = "You win!!! &#128513"
+  }
+
+  if (computerChoice === 'Scissors'){
+    opponentText.textContent = "Opponent chose Scissors"
+    winnerText.textContent = "It's a tie!"
+  }
+}
+
+
+//event listeners for each button 
+
+rockBtn.addEventListener("click", rockChosen);
+paperBtn.addEventListener("click", paperChosen);
+scissorsBtn.addEventListener("click", scissorsChosen);
