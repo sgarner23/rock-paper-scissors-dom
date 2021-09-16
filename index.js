a2f07ca8d5054187927d02765830fcf449b7ca35
Stephen Garner <stephenallen.garner@gmail.com>
2021-09-15 20:51:13 -0600

    added state engine for game reset

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-
-//checking to make sure file is connected 
+//checking to make sure file is connected
 console.log("Hello World");
 
-//defining global variables 
+//defining global variables
+let state = { userCanSelect: true };
+
 let rockBtn = document.querySelector(".rock-button");
 let paperBtn = document.querySelector(".paper-button");
 let scissorsBtn = document.querySelector(".scissors-button");
 let playAgainBtn = document.querySelector(".play-button");
 
 let opponentText = document.getElementById("computer-choice-text");
-let winnerText = document.getElementById('winner-text');
-let playerText = document.getElementById('player-text');
+let winnerText = document.getElementById("winner-text");
+let playerText = document.getElementById("player-text");
 
 let computerChoice = "";
 
-
-//function to calculate computer choice 
+//function to calculate computer choice
 function getComputerChoice() {
   let computerChoiceInt = Math.floor(Math.random() * 3);
   let opponentChoice = "";
@@ -38,73 +38,93 @@ function getComputerChoice() {
   return opponentChoice;
 }
 
-//calculate who wins if user clicks rock 
+//calculate who wins if user clicks rock
 function rockChosen(e) {
+  if (!state.userCanSelect) {
+    return;
+  }
+
+  state.userCanSelect = false;
+
   computerChoice = getComputerChoice();
-  playerText.textContent = "Player chose Rock"
+  playerText.textContent = "Player chose Rock";
   if (computerChoice === "Rock") {
     opponentText.textContent = "Opponent chose Rock";
-    winnerText.textContent = "It's a tie!"
-
+    winnerText.textContent = "It's a tie!";
   }
 
-  if (computerChoice === "Paper"){
+  if (computerChoice === "Paper") {
     opponentText.textContent = "Opponent chose Paper";
-    winnerText.textContent = "Opponent wins!"
+    winnerText.textContent = "Opponent wins!";
   }
 
-  if (computerChoice === 'Scissors'){
-    opponentText.textContent = "Opponent chose Scissors"
-    winnerText.innerHTML = "You win!!! &#128513"
+  if (computerChoice === "Scissors") {
+    opponentText.textContent = "Opponent chose Scissors";
+    winnerText.innerHTML = "You win!!! &#128513";
   }
 }
 
-//calculate who wins if user clicks paper 
-function paperChosen(e){
-  playerText.textContent = "Player chose Paper"
+//calculate who wins if user clicks paper
+function paperChosen(e) {
+  if (!state.userCanSelect) {
+    return;
+  }
+  state.userCanSelect = false;
+  playerText.textContent = "Player chose Paper";
   computerChoice = getComputerChoice();
   if (computerChoice === "Rock") {
     opponentText.textContent = "Opponent chose Rock";
-    winnerText.innerHTML = "You win!!! &#128513"
-
+    winnerText.innerHTML = "You win!!! &#128513";
   }
 
-  if (computerChoice === "Paper"){
+  if (computerChoice === "Paper") {
     opponentText.textContent = "Opponent chose Paper";
-    winnerText.textContent = "It's a tie!"
+    winnerText.textContent = "It's a tie!";
   }
 
-  if (computerChoice === 'Scissors'){
-    opponentText.textContent = "Opponent chose Scissors"
-    winnerText.textContent = "Opponent wins!"
+  if (computerChoice === "Scissors") {
+    opponentText.textContent = "Opponent chose Scissors";
+    winnerText.textContent = "Opponent wins!";
   }
 }
 
-//calculate who wins if user clicks scissors 
-function scissorsChosen(e){
-  playerText.textContent = "Player chose Scissors"
+//calculate who wins if user clicks scissors
+function scissorsChosen(e) {
+  if (!state.userCanSelect) {
+    return;
+  }
+  state.userCanSelect = false;
+
+  playerText.textContent = "Player chose Scissors";
   computerChoice = getComputerChoice();
   if (computerChoice === "Rock") {
     opponentText.textContent = "Opponent chose Rock";
-    winnerText.textContent = "Opponent wins!"
-
+    winnerText.textContent = "Opponent wins!";
   }
 
-  if (computerChoice === "Paper"){
+  if (computerChoice === "Paper") {
     opponentText.textContent = "Opponent chose Paper";
-    winnerText.textContent = "You win!!!" 
-    winnerText.innerHTML = "You win!!! &#128513"
+    winnerText.textContent = "You win!!!";
+    winnerText.innerHTML = "You win!!! &#128513";
   }
 
-  if (computerChoice === 'Scissors'){
-    opponentText.textContent = "Opponent chose Scissors"
-    winnerText.textContent = "It's a tie!"
+  if (computerChoice === "Scissors") {
+    opponentText.textContent = "Opponent chose Scissors";
+    winnerText.textContent = "It's a tie!";
   }
 }
 
+//reset game
+function playAgainBtnPushed() {
+  state.userCanSelect = true;
+  playerText.textContent = "Can";
+  opponentText.textContent = "You";
+  winnerText.textContent = "Win?";
+}
 
-//event listeners for each button 
+//event listeners for each button
 
 rockBtn.addEventListener("click", rockChosen);
 paperBtn.addEventListener("click", paperChosen);
 scissorsBtn.addEventListener("click", scissorsChosen);
+playAgainBtn.addEventListener("click", playAgainBtnPushed);
